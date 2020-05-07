4c141f026780acf622292ba9374c45e3b205be37
Luis <luis.olguin89@gmail.com>
2020-05-06 22:37:29 -0500

    Push Code to Grab Elements and Filter Data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -13,3 +13,35 @@ tableData.forEach((ufodata) => {
       cell.text(value);
     });
   });
+
+  var button = d3.select("#filter-btn");
+  var form = d3.select("form");
+
+  button.on("click", runQuery);
+  form.on("submit", runQuery);
+
+  function runQuery()
+  {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select(".form-control");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+
+    var filteredData = tableData.filter(data => data.datetime === inputValue);
+
+    console.log(filteredData);
+
+  };
+
+  function clearTable()
+  {
+    //insert code to clear table data
+
+
+  }
